1de5b7c2bf97b9de01e0f45c7844d25452c40e6b
cyyoung053 <39162803+cyyoung053@users.noreply.github.com>
2024-09-05 12:33:47 -0400

    Update script.js

--- script.js
+++ script.js
@@ -23,18 +23,20 @@ document.addEventListener("DOMContentLoaded", function() {
 });
 
 function checkCredentials() {
-const username = prompt("Enter username:");
-const password = prompt("Enter password:");
+    const username = prompt("Enter username:");
+    const password = prompt("Enter password:");
 
-// Static username and password
-const correctUsername = "marty";
-const correctPassword = "fluxcapacitor88";
+    // Static username and password
+    const correctUsername = "marty";
+    const correctPassword = "fluxcapacitor88";
 
-// Check if username and password are correct
-if (username === correctUsername && password === correctPassword) {
-    alert("Great Scott! You're in!");
-    window.location.href = "http://www.bttf.com"; // Replace with your desired URL
-} else {
-    alert("Whoa, Doc! Incorrect credentials!");
-}
+    // Check if username and password are correct
+    if (username === correctUsername && password === correctPassword) {
+        alert("Great Scott! You're in!");
+        // Set the authenticated flag in local storage
+        localStorage.setItem("authenticated", "true");
+        window.location.href = "home.html"; // Redirect to home.html after successful login
+    } else {
+        alert("Whoa, Doc! Incorrect credentials!");
+    }
 }
